787929aa31fddeb505498aa571d60eeba20602e8
Tom de Bruijn <tom@tomdebruijn.com>
2018-10-09 13:11:15 +0200

    Add Elixir example app

---
+++ README.md
@@ -0,0 +1,8 @@
+# Rust staticlib linking with Elixir issue example app
+
+## Elixir app
+
+```
+cd elixir_app
+mix run
+```
---
+++ elixir_app/mix.exs
@@ -0,0 +1,20 @@
+defmodule AppsignalElixirExample.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :elixir_app,
+     version: "0.1.0",
+     elixir: "~> 1.7",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps()]
+  end
+
+  def application do
+    [applications: [:logger]]
+  end
+
+  defp deps do
+    []
+  end
+end
